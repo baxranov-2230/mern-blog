9895d377d06c5bbb159e18b0311e800b9f05702b
baxranov-2230 <baxranovahror2230@gmail.com>
2024-02-05 22:03:36 +0500

    Controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(4100, () => {
   console.log("Server is running 3000!!");
 });
+
+app.use("/api/user", userRoutes);
